0018c8d9090b2e3b51daf31f99965f61b2cc61a2
ChoiDevv <oinkoink71@naver.com>
2023-09-03 17:47:24 +0900

    2023-09-03 장바구니, 상품 상세 페이지 css 수정

---
+++ src/main/java/com/shop/secondHands/order/controller/OrderController.java
@@ -0,0 +1,7 @@
+package com.shop.secondHands.order.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class OrderController {
+}
---
+++ src/main/java/com/shop/secondHands/order/service/OrderServiceImpl.java
@@ -0,0 +1,7 @@
+package com.shop.secondHands.order.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class OrderServiceImpl implements OrderService {
+}
